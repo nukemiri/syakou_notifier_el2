505284501f6f57372e0e0c24ae4be2163243490f
nukemiri <aid2001kengo@gmail.com>
2021-09-01 08:21:57 +0900

    自動URL生成

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 student_id = ""
 # パスワード
 password = ""
-# ログイン画面のURL内(abc=の後)にある自動車学校のID
-school_id = ""
+# ログイン画面のURL内のabc=の後の数値
+url = "https://www.e-license.jp/el26/pc/index.action?abc=mBvUy5Ul9%2F4%2BbrGQYS%2B1OA%3D%3D"
 # LINE Notifyのトークン
 line_token = ""
