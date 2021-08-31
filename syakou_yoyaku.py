1c26a09d17550e94133e9d5b3f0011743d9658e6
nukemiri <aid2001kengo@gmail.com>
2021-09-01 08:21:57 +0900

    ログインurlから各種パラメータを自動取得

--- syakou_yoyaku.py
+++ syakou_yoyaku.py
@@ -8,39 +8,47 @@ import datetime
 import requests
 from sendline import Line
 import pickle
-import colorama
 import config
+import colorama
 import os
+import re
+import urllib
 
 options = Options()
 #ヘッドレスモードで実行
 options.add_argument("--headless")
 driver = webdriver.Chrome(ChromeDriverManager().install(),options=options)
-urls = ["https://www.e-license.jp/el2/pc/p01a.action?b.studentId="+config.student_id+"&b.password="+config.password+"&method%3AdoLogin=%83%8D%83O%83C%83%93&b.wordsStudentNo=%8B%B3%8FK%90%B6%94%D4%8D%86&b.processCd=&b.kamokuCd=&b.schoolCd="+config.school_id+"&index=&server=el1",
-"https://www.e-license.jp/el2/pc/p03a.action?b.schoolCd="+config.school_id+"&b.processCd=N&b.kamokuCd=0&b.lastScreenCd=&b.instructorTypeCd=0&b.dateInformationType=&b.infoPeriodNumber=&b.carModelCd=301&b.instructorCd=0&b.page=1&b.groupCd=1&b.changeInstructorFlg=1&b.nominationInstructorCd=0",
-"https://www.e-license.jp/el2/pc/p03a.action?b.schoolCd="+config.school_id+"&b.processCd=N&b.kamokuCd=0&b.lastScreenCd=&b.instructorTypeCd=0&b.dateInformationType=&b.infoPeriodNumber=&b.carModelCd=301&b.instructorCd=0&b.page=2&b.groupCd=1&b.changeInstructorFlg=1&b.nominationInstructorCd=0",
-"https://www.e-license.jp/el2/pc/p03a.action?b.schoolCd="+config.school_id+"&b.processCd=N&b.kamokuCd=0&b.lastScreenCd=&b.instructorTypeCd=0&b.dateInformationType=&b.infoPeriodNumber=&b.carModelCd=301&b.instructorCd=0&b.page=3&b.groupCd=1&b.changeInstructorFlg=1&b.nominationInstructorCd=0"]
+url = config.url
+driver.get(url)
+login_id = driver.find_element_by_id("p01aForm_b_studentId")
+login_id.send_keys(config.student_id)
+password = driver.find_element_by_id("p01aForm_b_password")
+password.send_keys(config.password)
+login_btn = driver.find_element_by_id("p01aForm_login")
+login_btn.click()
 
-avadates = []
-avadatetimes =[]
 f = open(os.path.join(os.path.dirname(__file__),"emptytimelist.txt"),"rb")
 try:
     prelist = pickle.load(f)
 except:
     prelist = []
-    
+
+avadates = []
+avadatetimes =[]
 notify_status = False
 
-for url in urls:
-    driver.get(url)
+# 自動ログイン用のURL生成
+version = re.findall("(?<=\/el).+?(?=\/)", url)[0]
+school_id = urllib.parse.parse_qs(urllib.parse.urlparse(url).query)["abc"][0]
+d={'b.studentId': config.student_id, 'b.password': config.password, 'method:doLogin': "ログイン", 'b.wordsStudentNo': '教習生番号', 'b.schoolCd': school_id, 'index': '0'}
+one_tap_url = f"https://www.e-license.jp/el{version}/pc/p01a.action?{urllib.parse.urlencode(d)}"
+
+while True:
     html=driver.page_source
     soup = BeautifulSoup(html, "html.parser")
     datelist = soup.find_all(class_="date")
     for date in datelist:
         emptynum = len(date.find_all(class_="status1"))
-        
-        # if len(date.find_all(class_="status3"))+len(date.find_all(class_="status4"))>=2: 
-        #     print(date.find(class_="view").text.replace("\n","").replace("\t","")+": 済")
         if emptynum==0:
             print(date.find(class_="view").text.replace("\n","").replace("\t","")+": 満")
         else:
@@ -53,6 +61,12 @@ for url in urls:
                     avadatetimes.append(date.find(class_="view").text.replace("\n","").replace("\t","")+" "+str(count))
                 count += 1
             avadates.append(date.find(class_="view").text.replace("\n","").replace("\t","")+" "+" ".join(emptylist))
+
+    try:
+        next_btn = driver.find_element_by_xpath("//*[@alt='次週へ']")
+        next_btn.click()
+    except:
+        break
 driver.quit()
 
 for i in avadatetimes:
@@ -65,7 +79,7 @@ if len(avadates)==0:
     print("*********\n空きなし\n*********")
 else:
     print("*********\n空きあり\n"+"\n".join(avadates)+"\n*********")
-    message = ("技能予約可能\n\n"+"\n".join(avadates)+"\n"+urls[0])
+    message = ("技能予約可能\n\n"+"\n".join(avadates)+"\n"+one_tap_url)
     if notify_status:
         line = Line(config.line_token)
 
